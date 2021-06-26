377151bf0f42c62fa7f6c43cb5ad024221f8083b
Ofek Atar <ofekatar98@gmail.com>
2021-06-26 18:54:22 +0300

    Fixed app config bug.

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -1,11 +1,10 @@
 import { extractExistingProperties } from 'app/common/utils/objects';
-import 'dotenv/register';
 
-const extractedConfig = extractExistingProperties(process.env, 'WEATHER_API_KEY', 'NODE_ENV');
+const extractedConfig = extractExistingProperties(process.env, 'REACT_APP_WEATHER_API_KEY', 'NODE_ENV');
 
 const config = {
     weatherApi: {
-        key: extractedConfig.WEATHER_API_KEY
+        key: extractedConfig.REACT_APP_WEATHER_API_KEY
     },
 }
 
